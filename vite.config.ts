3b683d3b72af6b3d7bac57c3de5963dc6b3fec51
huytq <huytq@beamewa.com.vn>
2026-01-20 15:12:50 +0700

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,31 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      '@': path.resolve('src'),
-      '@assets': path.resolve('src/assets'),
-      '@components': path.resolve('src/components'),
-      '@pages': path.resolve('src/pages'),
-      '@hooks': path.resolve('src/hooks'),
-      '@utils': path.resolve('src/utils'),
-      '@locales': path.resolve('src/locales'),
-      '@services': path.resolve('src/services'),
-      '@types': path.resolve('src/types'),
-      '@features': path.resolve('src/features'),
-      '@constants': path.resolve('src/constants')
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return {
+    plugins: [react(), tailwindcss()],
+    server: {
+      port: Number(env.VITE_PORT) || 3000,
+    },
+    resolve: {
+      alias: {
+        '@': path.resolve('src'),
+        '@assets': path.resolve('src/assets'),
+        '@components': path.resolve('src/components'),
+        '@pages': path.resolve('src/pages'),
+        '@hooks': path.resolve('src/hooks'),
+        '@utils': path.resolve('src/utils'),
+        '@locales': path.resolve('src/locales'),
+        '@services': path.resolve('src/services'),
+        '@types': path.resolve('src/types'),
+        '@features': path.resolve('src/features'),
+        '@constants': path.resolve('src/constants')
+      }
     }
   }
 })
